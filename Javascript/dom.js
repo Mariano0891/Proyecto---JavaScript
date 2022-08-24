d5e5469da2b476cf66ae8bb984f64d267b7eac45
Mariano Pavone <mariano0891@gmail.com>
2022-08-24 07:01:40 -0300

    Creados las funciones que generan el arrays de funciones disponibles de cada pelicula

--- Javascript/dom.js
+++ Javascript/dom.js
@@ -3,8 +3,13 @@ const containerPeliculas = document.getElementById('container__peliculas');
 
 const containerCombos = document.getElementById('container__combos');
 
+const formulario = document.getElementById ('formulario__registro');
 
-cargarPeliculas (peliculas)
+crearFunciones ();
+
+crearFuncionesPorPeliculas (peliculas);
+
+cargarPeliculas (peliculas);
 
 function cargarPeliculas() {
     peliculas.forEach(pelicula => {
@@ -25,6 +30,16 @@ function cargarPeliculas() {
     let botonComprar = document.getElementById (`botonComprar${pelicula.id}`)
     botonComprar.addEventListener ('click', () => {
         console.log(pelicula.titulo)
+        let div = document.createElement('div')
+        div.className = 'pelicula'
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+                            <img src="${pelicula.imagen}" class="card-img-top" alt="${pelicula.titulo}">
+                            <div class="card-body">
+                                <h5 class="card-title">${pelicula.titulo}</h5>
+                                <p class="card-text">${pelicula.genero}</p>
+                                <p class="card-text">${pelicula.clasificacion}</p>
+                            </div>
+                        </div>`
         document.getElementById('container__peliculas').style.display = 'none'
         cargarCombos (combos)
     })
@@ -47,3 +62,31 @@ function cargarCombos () {
     containerCombos.appendChild(div)
     })
 }
+
+/*let linkRegistro = document.getElementById('registrarse')
+linkRegistro.addEventListener ('click', () => {
+    console.log(`Acceso al registro`)
+    document.getElementById('container__peliculas').style.display = 'none'
+    formularioRegistro (linkRegistro)
+})
+
+function formularioRegistro () {
+    let form = document.createElement ('form action="#"')
+    form.className = 'formularioRegistro'
+    form.innerHTML = `<label for="nombre">Nombre</label>
+                    <input type="text" id="nombre" required>
+                    <label for="apellido">Apellido</label>
+                    <input type="text" id="apellido">
+                    <label for="email">Mail</label>
+                    <input type="email" id="email" required>
+                    <label for="telefono">Telefono</label>
+                    <input type="tel" id="telefono">
+                    <label for="contraseña" required>Contraseña</label>
+                    <input type="password" id="contraseña">
+                    <label for="repetirContraseña"required>Repita contraseña</label>
+                    <input type="password" id="repitaContraseña">
+                    <input type="submit" value="Enviar" id="botonEnvio">                 
+                `
+    formulario.appendChild(div)
+}*/
+
--- Javascript/variables.js
+++ Javascript/variables.js
@@ -2,6 +2,8 @@
 
 const precioEntradas = 500;
 
+// Array de peliculas
+
 class pelicula{
     constructor(id, titulo, genero, clasificacion, imagen){
         this.id = id;
@@ -25,6 +27,73 @@ let pelicula10 = new pelicula (10, "Minions: nace un villano", "animación", "AT
 
 let peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8, pelicula9, pelicula10];
 
+// Array de salas y horarios (por el momento lo voy a plantear como si hubiera una sala exclusiva para cada pelicula)
+
+// Primero creo las variables de horarios
+
+class horario{
+    constructor(horarioInicio, capacidad){
+        this.horarioInicio = horarioInicio;
+        this.capacidad = capacidad;
+    }
+}
+
+let horario1 = new horario (`12:00`, 50);
+let horario2 = new horario (`15:00`, 50);
+let horario3 = new horario (`18:00`, 50);
+let horario4 = new horario (`21:00`, 50);
+
+let horarios = [horario1, horario2, horario3, horario4];
+
+// Ahora la constante de los dias de la semana
+
+const diasDeLaSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
+// Aqui se crea el array de las funciones por dia donde a cada uno se le cargan todos los horarios de funcion
+
+class funcion{
+    constructor(dia, horarioInicio, capacidad){
+        this.dia = dia;
+        this.horarioInicio = horarioInicio;
+        this.capacidad = capacidad;
+    }
+}
+
+let funciones = []
+
+function crearFunciones () {
+    diasDeLaSemana.forEach(dia => {
+        funciones.push (new funcion (dia, horario1.horarioInicio,horario1.capacidad))
+        funciones.push (new funcion (dia, horario2.horarioInicio,horario2.capacidad))
+        funciones.push (new funcion (dia, horario3.horarioInicio,horario3.capacidad))
+        funciones.push (new funcion (dia, horario4.horarioInicio,horario4.capacidad))
+    })
+    console.log (funciones)
+}
+
+// Una vez definidos todos los horarios para cada dia se crea un nuevo array donde tendremos por cada pelicula las 4 funciones disponibles por dia
+
+class funcionPorPelicula{
+    constructor(titulo, dia, horarioInicio, capacidad){
+        this.titulo = titulo;
+        this.dia = dia;
+        this.horarioInicio = horarioInicio;
+        this.capacidad = capacidad;
+    }
+}
+
+let funcionesPorPeliculas = []
+
+function crearFuncionesPorPeliculas () {
+    peliculas.forEach(pelicula => {
+        funciones.forEach(funcion => {
+            funcionesPorPeliculas.push (new funcionPorPelicula (pelicula.titulo, funcion.dia, funcion.horarioInicio, funcion.capacidad))
+        })
+    })
+    console.log (funcionesPorPeliculas)
+}
+
+// Array de combos
 
 class combo {
     constructor (id, descripcion, precio){
@@ -40,6 +109,8 @@ let combo3 = new combo (3, "Golosinas + bebida", 1000);
 let combo4 = new combo (4, "Snack + bebida", 1200);
 
 let combos = [combo1, combo2, combo3, combo4];
+
+// Array de tamaños de combos
 
 class tamaño {
     constructor (id, especificacion, factorModificador){
@@ -58,3 +129,19 @@ let tamaños = [tamaño1, tamaño2, tamaño3];
 let compra  = [];
 
 let compraCombos = [];
+
+// Array de usuarios
+
+let usuarios = [];
+
+class usuario {
+    constructor (nombre, apellido, mail, telefono, contraseña, preguntaSeguridad, respuestaSeguridad){
+        this.nombre = nombre;
+        this. apellido = apellido;
+        this.mail = mail;
+        this.telefono = telefono;
+        this.contraseña = contraseña;
+        this.preguntaSeguridad = preguntaSeguridad;
+        this.respuestaSeguridad = respuestaSeguridad;
+    }
+}
